caf8e4285add3d180a95738176efb08d21428a29
mgood7123 <smallville7123@gmail.com>
2023-10-16 09:59:49 +1000

    add example with debug information

--- example.cpp
+++ example.cpp
@@ -1,202 +1,257 @@
-//===----- ExampleModules.h - IR modules for LLJIT examples -----*- C++ -*-===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// Example modules for LLJIT examples
-//
-//===----------------------------------------------------------------------===//
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ExecutionEngine/Orc/ThreadSafeModule.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/ADT/StringMap.h>
+#include <llvm/ExecutionEngine/JITEventListener.h>
+#include <llvm/ExecutionEngine/JITLink/JITLinkMemoryManager.h>
+#include <llvm/ExecutionEngine/Orc/ExecutionUtils.h>
+#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/ExecutionEngine/Orc/TargetProcess/TargetExecutionUtils.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/raw_ostream.h>
 
-#ifndef LLVM_EXAMPLES_ORCV2EXAMPLES_EXAMPLEMODULES_H
-#define LLVM_EXAMPLES_ORCV2EXAMPLES_EXAMPLEMODULES_H
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DebugInfoMetadata.h>
+#include <llvm/IR/DIBuilder.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/InstrTypes.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
 
-#include "llvm/ADT/StringRef.h"
-#include "llvm/ExecutionEngine/Orc/ThreadSafeModule.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IRReader/IRReader.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Error.h"
-#include "llvm/Support/SourceMgr.h"
-
-const llvm::StringRef Add1Example =
-    R"(
-  define i32 @add1(i32 %x) {
-  entry:
-    %r = add nsw i32 %x, 1
-    ret i32 %r
-  }
-)";
-
-inline llvm::Error createSMDiagnosticError(llvm::SMDiagnostic &Diag) {
-  using namespace llvm;
-  std::string Msg;
-  {
-    raw_string_ostream OS(Msg);
-    Diag.print("", OS);
-  }
-  return make_error<StringError>(std::move(Msg), inconvertibleErrorCode());
-}
-
-inline llvm::Expected<llvm::orc::ThreadSafeModule>
-parseExampleModule(llvm::StringRef Source, llvm::StringRef Name) {
-  using namespace llvm;
-  auto Ctx = std::make_unique<LLVMContext>();
-  SMDiagnostic Err;
-  if (auto M = parseIR(MemoryBufferRef(Source, Name), Err, *Ctx))
-    return orc::ThreadSafeModule(std::move(M), std::move(Ctx));
-
-  return createSMDiagnosticError(Err);
-}
-
-inline llvm::Expected<llvm::orc::ThreadSafeModule>
-parseExampleModuleFromFile(llvm::StringRef FileName) {
-  using namespace llvm;
-  auto Ctx = std::make_unique<LLVMContext>();
-  SMDiagnostic Err;
-
-  if (auto M = parseIRFile(FileName, Err, *Ctx))
-    return orc::ThreadSafeModule(std::move(M), std::move(Ctx));
-
-  return createSMDiagnosticError(Err);
-}
-
-#endif // LLVM_EXAMPLES_ORCV2EXAMPLES_EXAMPLEMODULES_H
-
-//===--------------- LLJITWithGDBRegistrationListener.cpp -----------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This file shows how to switch LLJIT to use a custom object linking layer (we
-// use ObjectLinkingLayer, which is backed by JITLink, as an example).
-//
-//===----------------------------------------------------------------------===//
-
-#include "llvm/ADT/StringMap.h"
-#include "llvm/ExecutionEngine/JITEventListener.h"
-#include "llvm/ExecutionEngine/JITLink/JITLinkMemoryManager.h"
-#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
-#include "llvm/ExecutionEngine/Orc/LLJIT.h"
-#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
-#include "llvm/ExecutionEngine/Orc/TargetProcess/TargetExecutionUtils.h"
-#include "llvm/ExecutionEngine/SectionMemoryManager.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/InitLLVM.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/raw_ostream.h"
-
-// additional --version info
+// additional info for --version
 #include <llvm/MC/TargetRegistry.h>
 #include <llvm/TargetParser/Host.h>
 
-using namespace llvm;
-using namespace llvm::orc;
+llvm::ExitOnError ExitOnErr;
 
-ExitOnError ExitOnErr;
+struct ModuleHolder {
+    std::unique_ptr<llvm::LLVMContext> context;
+    std::unique_ptr<llvm::Module> module;
 
-static cl::opt<std::string>
-    EntryPointName("entry", cl::desc("Symbol to call as main entry point"),
-                   cl::init("main"));
-
-static cl::list<std::string> InputFiles(cl::Positional, cl::OneOrMore,
-                                        cl::desc("input files"));
-
-static cl::list<std::string> InputArgv("args", cl::Positional,
-                                       cl::desc("<program arguments>..."),
-                                       cl::PositionalEatsArgs);
-
-int main(int argc, char *argv[]) {
-  // Initialize LLVM.
-  InitLLVM X(argc, argv);
-
-  InitializeAllTargetInfos();
-  InitializeAllTargets();
-  InitializeAllTargetMCs();
-  InitializeAllAsmPrinters();
-  InitializeAllAsmParsers();
-  InitializeAllDisassemblers();
-
-  InitializeNativeTarget();
-  InitializeNativeTargetAsmPrinter();
-  InitializeNativeTargetAsmParser();
-  InitializeNativeTargetDisassembler();
-  InitializeAllTargetMCAs();
-
-  // Register the Target and CPU printer for --version.
-  cl::AddExtraVersionPrinter(llvm::sys::printDefaultTargetAndDetectedCPU);
-  // Register the target printer for --version.
-  cl::AddExtraVersionPrinter(llvm::TargetRegistry::printRegisteredTargetsForVersion);
-
-  cl::ParseCommandLineOptions(argc, argv, "LLJITWithGDBRegistrationListener");
-  ExitOnErr.setBanner(std::string(argv[0]) + ": ");
-
-  // Detect the host and set code model to small.
-  auto JTMB = ExitOnErr(JITTargetMachineBuilder::detectHost());
-  if (!JTMB.getTargetTriple().isOSLinux())
-    errs()
-        << "Warning: This demo may not work for platforms other than Linux.\n";
-
-  // Create an LLJIT instance and use a custom object linking layer creator to
-  // register the GDBRegistrationListener with our RTDyldObjectLinkingLayer.
-  auto J =
-      ExitOnErr(LLJITBuilder()
-                    .setJITTargetMachineBuilder(std::move(JTMB))
-                    .setObjectLinkingLayerCreator([&](ExecutionSession &ES,
-                                                      const Triple &TT) {
-                      auto GetMemMgr = []() {
-                        return std::make_unique<SectionMemoryManager>();
-                      };
-                      auto ObjLinkingLayer =
-                          std::make_unique<RTDyldObjectLinkingLayer>(
-                              ES, std::move(GetMemMgr));
-
-                      // Register the event listener.
-                      ObjLinkingLayer->registerJITEventListener(
-                          *JITEventListener::createGDBRegistrationListener());
-
-                      // Make sure the debug info sections aren't stripped.
-                      ObjLinkingLayer->setProcessAllSections(true);
-
-                      return ObjLinkingLayer;
-                    })
-                    .create());
-
-  // Make sure that our process symbols are visible to JIT'd code.
-  {
-    MangleAndInterner Mangle(J->getExecutionSession(), J->getDataLayout());
-    J->getMainJITDylib().addGenerator(
-        ExitOnErr(orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
-            J->getDataLayout().getGlobalPrefix(),
-            [MainName = Mangle("main")](const orc::SymbolStringPtr &Name) {
-              return Name != MainName;
-            })));
-  }
-
-  // Load the input modules.
-  for (auto &InputFile : InputFiles) {
-    auto Ctx = std::make_unique<LLVMContext>();
-    SMDiagnostic Err;
-    std::unique_ptr<Module> M = parseIRFile(InputFile, Err, *Ctx);
-    if (!M) {
-      Err.print(argv[0], errs());
-      return 1;
+    ModuleHolder(const char * name) {
+        context = std::make_unique<llvm::LLVMContext>();
+        module = std::make_unique<llvm::Module>(name, *context);
     }
 
-    ExitOnErr(J->addIRModule(ThreadSafeModule(std::move(M), std::move(Ctx))));
-  }
+    llvm::orc::ThreadSafeModule create() {
+        return std::move(llvm::orc::ThreadSafeModule(std::move(module), std::move(context)));
+    }
+};
 
-  // Look up the entry point, cast it to a C main function pointer, then use
-  // runAsMain to call it.
-  auto EntryAddr = ExitOnErr(J->lookup(EntryPointName));
-  auto EntryFn = EntryAddr.toPtr<int(int, char *[])>();
+int main(int argc, char *argv[]) {
+    // Initialize LLVM.
+    llvm::InitLLVM X(argc, argv);
 
-  return runAsMain(EntryFn, InputArgv, StringRef(InputFiles.front()));
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmPrinters();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllDisassemblers();
+
+    llvm::InitializeNativeTarget();
+    llvm::InitializeNativeTargetAsmPrinter();
+    llvm::InitializeNativeTargetAsmParser();
+    llvm::InitializeNativeTargetDisassembler();
+    llvm::InitializeAllTargetMCAs();
+
+    // Register the Target and CPU printer for --version.
+    llvm::cl::AddExtraVersionPrinter(llvm::sys::printDefaultTargetAndDetectedCPU);
+    // Register the target printer for --version.
+    llvm::cl::AddExtraVersionPrinter(llvm::TargetRegistry::printRegisteredTargetsForVersion);
+
+    llvm::cl::ParseCommandLineOptions(argc, argv, "LLJITWithInitializers");
+    ExitOnErr.setBanner(std::string(argv[0]) + ": ");
+
+    // Detect the host and set code model to small.
+    auto JTMB = ExitOnErr(llvm::orc::JITTargetMachineBuilder::detectHost());
+    if (!JTMB.getTargetTriple().isOSLinux())
+        llvm::errs() << "Warning: This demo may not work for platforms other than Linux.\n";
+
+    // Create an LLJIT instance and use a custom object linking layer creator to
+    // register the GDBRegistrationListener with our RTDyldObjectLinkingLayer.
+    auto J =
+        ExitOnErr(llvm::orc::LLJITBuilder()
+                        .setJITTargetMachineBuilder(std::move(JTMB))
+                        .setObjectLinkingLayerCreator([&](llvm::orc::ExecutionSession &ES,
+                                                        const llvm::Triple &TT) {
+                        auto GetMemMgr = []() {
+                            return std::make_unique<llvm::SectionMemoryManager>();
+                        };
+                        auto ObjLinkingLayer =
+                            std::make_unique<llvm::orc::RTDyldObjectLinkingLayer>(
+                                ES, std::move(GetMemMgr));
+
+                        // Register the event listener.
+                        ObjLinkingLayer->registerJITEventListener(
+                            *llvm::JITEventListener::createGDBRegistrationListener());
+
+                        // Make sure the debug info sections aren't stripped.
+                        ObjLinkingLayer->setProcessAllSections(true);
+
+                        return ObjLinkingLayer;
+                        })
+                        .create());
+
+    /*
+
+    // Make sure that our process symbols are visible to JIT'd code.
+    {
+        MangleAndInterner Mangle(J->getExecutionSession(), J->getDataLayout());
+        J->getMainJITDylib().addGenerator(
+            ExitOnErr(orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(
+                J->getDataLayout().getGlobalPrefix(),
+                [MainName = Mangle("main")](const orc::SymbolStringPtr &Name) {
+                return Name != MainName;
+                })));
+    }
+
+    // Load the input modules.
+    for (auto &InputFile : InputFiles) {
+        auto Ctx = std::make_unique<LLVMContext>();
+        SMDiagnostic Err;
+        std::unique_ptr<Module> M = parseIRFile(InputFile, Err, *Ctx);
+        if (!M) {
+        Err.print(argv[0], errs());
+        return 1;
+        }
+
+        ExitOnErr(
+        J->addIRModule(
+            ThreadSafeModule(std::move(M), std::move(Ctx))
+        )
+        );
+    }
+    */
+
+    // Load the module.
+
+    // ModuleHolder mb("example.c");
+    std::unique_ptr<llvm::LLVMContext> context;
+    std::unique_ptr<llvm::Module> module;
+
+    context = std::make_unique<llvm::LLVMContext>();
+    module = std::make_unique<llvm::Module>("example.c", *context);
+
+    const char * func_name = "main2";
+
+    // add debug info
+    llvm::DIBuilder debug(*module);
+    auto debug_file = debug.createFile("example.c", "/home/DATA/git/Gecko/HTML/qparse/ManagedObject/llvm_mcjit");
+    auto cu = debug.createCompileUnit(
+        llvm::dwarf::DW_LANG_C99, // Lang
+        debug_file,               // File
+        "LLVM", // Producer
+        false, // isOptimized
+        "", // Flags
+        0, // RV
+        llvm::StringRef(), // SplitName
+        llvm::DICompileUnit::DebugEmissionKind::FullDebug, // Kind
+        0, // DWOld
+        false, // DebugSplitInlining
+        false, // DebugInfoForProfiling
+        llvm::DICompileUnit::DebugNameTableKind::None, // NameTableKind
+        false // RangesBaseAddress
+    );
+    module->addModuleFlag(llvm::Module::Warning, "Debug Info Version", llvm::DEBUG_METADATA_VERSION);
+    // mac os only supports dwarf2.
+    module->addModuleFlag(llvm::Module::Warning, "Dwarf Version", 2);
+
+    auto func_type = llvm::FunctionType::get(llvm::Type::getInt32Ty(*context), /*not vararg*/false);
+    auto func = llvm::Function::Create(func_type, llvm::Function::ExternalLinkage, func_name, *module);
+
+    // debug info - func_type
+    auto debug_int_type = debug.createBasicType("int", 32, llvm::dwarf::DW_ATE_signed);
+    llvm::SmallVector<llvm::Metadata*, 1> debug_func_array;
+    debug_func_array.push_back(debug_int_type);
+    auto debug_func_type_array = debug.getOrCreateTypeArray(debug_func_array);
+    auto debug_func_type = debug.createSubroutineType(debug_func_type_array);
+
+    // debug info - func
+    llvm::DIScope* debug_scope = (llvm::DIScope*) debug_file;
+    unsigned line_no = 2;
+    unsigned scope_line = 1;
+    auto debug_func_subprogram = debug.createFunction(
+        debug_file, func_name, llvm::StringRef(), debug_file,
+        line_no, debug_func_type, scope_line,
+        llvm::DINode::FlagZero, llvm::DISubprogram::SPFlagDefinition
+    );
+    func->setSubprogram(debug_func_subprogram);
+
+    // function body
+    {
+        auto basic_block = llvm::BasicBlock::Create(*context, "EntryBlock", func);
+        auto value_two = llvm::ConstantInt::get(llvm::Type::getInt32Ty(*context), 2);
+        auto r = llvm::ReturnInst::Create(*context, value_two);
+        r->setDebugLoc(llvm::DebugLoc(llvm::DILocation::get(*context, 1, 1, debug_func_subprogram)));
+        r->insertInto(basic_block, basic_block->end());
+    }
+
+    debug.finalize();
+    llvm::errs() << "printing module...\n";
+    module->print(llvm::errs(), nullptr);
+    llvm::errs() << "printed module...\n\n";
+
+    // auto m = mb.create();
+    auto m = std::move(llvm::orc::ThreadSafeModule(std::move(module), std::move(context)));
+    ExitOnErr(J->addIRModule(std::move(m)));
+
+    // auto Ctx = std::make_unique<llvm::LLVMContext>();
+    // llvm::SMDiagnostic Err;
+    // std::unique_ptr<llvm::Module> M = llvm::parseIRFile("./example.clang.ll", Err, *Ctx);
+    // if (!M) {
+    //     Err.print(argv[0], llvm::errs());
+    //         return 1;
+    // }
+
+    // ExitOnErr(
+    //     J->addIRModule(
+    //         llvm::orc::ThreadSafeModule(std::move(M), std::move(Ctx))
+    //     )
+    // );
+
+    // Look up the JIT'd function, cast it to a function pointer, then call it.
+    auto main_addr = ExitOnErr(J->lookup(func_name));
+    int (*main_func)(void) = main_addr.toPtr<int(void)>();
+
+    int res = main_func();
+    llvm::outs() << func_name << "() = " << res << "\n";
+
+    // int32_t InitializersRunFlag = 0;
+    // int32_t DeinitializersRunFlag = 0;
+
+    // ExitOnErr(J->getMainJITDylib().define(
+    //     llvm::orc::absoluteSymbols({{J->mangleAndIntern("InitializersRunFlag"),
+    //                       {llvm::orc::ExecutorAddr::fromPtr(&InitializersRunFlag),
+    //                        llvm::JITSymbolFlags::Exported}},
+    //                      {J->mangleAndIntern("DeinitializersRunFlag"),
+    //                       {llvm::orc::ExecutorAddr::fromPtr(&DeinitializersRunFlag),
+    //                        llvm::JITSymbolFlags::Exported}}})));
+
+    // Run static initializers.
+    // ExitOnErr(J->initialize(J->getMainJITDylib()));
+
+    // Run deinitializers.
+    // ExitOnErr(J->deinitialize(J->getMainJITDylib()));
+
+    // llvm::outs() << "InitializerRanFlag = " << InitializersRunFlag << "\n"
+    //        << "DeinitializersRunFlag = " << DeinitializersRunFlag << "\n";
+
+    llvm::llvm_shutdown();
+
+    return 0;
 }
-
