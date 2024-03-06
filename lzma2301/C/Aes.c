09746a3ac26946363bcf8e678bddc1d79a919d8e
mgood7123 <smallville7123@gmail.com>
2024-03-06 11:55:19 +1000

    disable AES on android arm/arm64

--- lzma2301/C/Aes.c
+++ lzma2301/C/Aes.c
@@ -55,7 +55,7 @@ static Byte InvS[256];
 
 #ifdef MY_CPU_X86_OR_AMD64
   #define USE_HW_AES
-#elif defined(MY_CPU_ARM_OR_ARM64) && defined(MY_CPU_LE)
+#elif defined(MY_CPU_ARM_OR_ARM64) && defined(MY_CPU_LE) && ! defined(__ANDROID__)
   #if defined(__clang__)
     #if (__clang_major__ >= 8) // fix that check
       #define USE_HW_AES
